f9a9ca5d3eec26759717bd23ceb413f32cff9b76
fengjunpku <fengjun2013@pku.edu.cn>
2016-08-15 01:19:21 +0800

    ignorefile

--- version1.0/build/CMakeFiles/ciae13c.dir/DependInfo.cmake
+++ version1.0/build/CMakeFiles/ciae13c.dir/DependInfo.cmake
@@ -4,24 +4,23 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/fengjun/CodesAtGitHub/13C/ciae13c.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/ciae13c.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/EmittingTreeRecorder.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/EmittingTreeRecorder.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/JunParMan.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/JunParMan.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/JunPrimaryGeneratorAction.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/JunPrimaryGeneratorAction.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/myDetectorConstruction.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/myDetectorConstruction.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/myEventAction.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/myEventAction.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/myHit.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/myHit.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/myPhysicsList.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/myPhysicsList.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/myRunAction.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/myRunAction.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/mySSD.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/mySSD.cc.o"
-  "/Users/fengjun/CodesAtGitHub/13C/src/myTTreeRecorder.cc" "/Users/fengjun/CodesAtGitHub/13C/build/CMakeFiles/ciae13c.dir/src/myTTreeRecorder.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/ciae13c.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/ciae13c.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/EmittingTreeRecorder.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/EmittingTreeRecorder.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/JunParMan.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/JunParMan.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/JunPrimaryGeneratorAction.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/JunPrimaryGeneratorAction.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/myDetectorConstruction.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/myDetectorConstruction.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/myEventAction.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/myEventAction.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/myHit.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/myHit.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/myPhysicsList.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/myPhysicsList.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/myRunAction.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/myRunAction.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/mySSD.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/mySSD.cc.o"
+  "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/src/myTTreeRecorder.cc" "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/CMakeFiles/ciae13c.dir/src/myTTreeRecorder.cc.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
   "G4INTY_USE_QT"
-  "G4INTY_USE_XT"
   "G4MULTITHREADED"
   "G4UI_USE"
   "G4UI_USE_QT"
@@ -30,22 +29,17 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
   "G4VIS_USE"
   "G4VIS_USE_OPENGL"
   "G4VIS_USE_OPENGLQT"
-  "G4VIS_USE_OPENGLX"
-  "G4VIS_USE_RAYTRACERX"
   "G4_STORE_TRAJECTORY"
   )
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/Users/fengjun/software/geant410/include/Geant4"
-  "/usr/local/include"
-  "/usr/local/Cellar/qt/4.8.7_2/include"
-  "/usr/local/Cellar/qt/4.8.7_2/lib/QtCore.framework"
-  "/usr/local/Cellar/qt/4.8.7_2/lib/QtCore.framework/Headers"
-  "/usr/local/Cellar/qt/4.8.7_2/include/QtGui"
-  "/usr/local/Cellar/qt/4.8.7_2/include/QtOpenGL"
+  "/usr/local/geant4/include/Geant4"
+  "/usr/include/QtCore"
+  "/usr/include/QtGui"
+  "/usr/include/QtOpenGL"
   "../include"
-  "/Users/fengjun/software/root534/include"
+  "/home/fengjun/Apps/root/include"
   )
 
 # Targets to which this target links.
--- version1.0/build/cmake_install.cmake
+++ version1.0/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/fengjun/CodesAtGitHub/13C
+# Install script for directory: /home/fengjun/CodesAtPKU/simu_ciae13c/version1.0
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -35,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/fengjun/CodesAtGitHub/13C/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/fengjun/CodesAtPKU/simu_ciae13c/version1.0/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
